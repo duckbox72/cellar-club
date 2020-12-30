3e3c8d37bfe2bc797ddb2f895036c12a1f8e2658
Luis Felipe Klaus <duckbox@hotmail.com>
2020-12-30 11:26:55 -0300

    serializers.py, views.py and urls.py updated

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,15 @@
-from rest_framework import serializers
+from rest_framework import fields, serializers
 from .models import Sample
 
+# GET request serializer (serializes response)
 class SampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sample
         fields = ('id', 'a', 'b', 'c')
+
+
+# POST request serializer (serializes request)
+class CreateSampleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sample
+        fields = ('a', 'b', 'c')
--- api/views.py
+++ api/views.py
@@ -1,14 +1,21 @@
-from .serializers import SampleSerializer
-from .models import Sample
 from django.shortcuts import render
-from rest_framework import generics   # creates a class from a generic API view
+
+# generics classes from a generic view, access to HTTP status codes
+from rest_framework import generics, serializers, status 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Sample
-from .serializers import SampleSerializer
-
-# Create your views here.
+from .serializers import *
 
 class SampleView(generics.ListAPIView):
     queryset = Sample.objects.all()
     serializer_class = SampleSerializer
+
+
+class CreateSampleView(APIView):
+    serializer_class = CreateSampleSerializer
+
+    def post(self, request, format=None):
+        pass
     
