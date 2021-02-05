a9e27fc52224002272a029a01428ab507a0a0aef
duckbox72 <duckbox@hotmail.com>
2021-02-04 21:20:42 -0300

    signup page, links to signin and signup match

---
+++ frontend/src/components/Signin.js
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        CellarClub.com
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: "url(/static/images/toast.jpg)",
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    //background: 'linear-gradient(225deg, #fff3e0 10%, #ffffff 100%)',
+    height: '100vh',
+    //margin: theme.spacing(8, 4),
+    padding: theme.spacing(8,4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(2),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn(props) {
+  const classes = useStyles();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignInButton = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      })
+    }
+    fetch('/api/signin', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      if (data.success) {
+        props.parentCallback(true)
+        props.history.push('/') 
+      } else {
+        setUsername('');
+        setPassword('');
+        alert(data.message)
+      }
+      
+    });
+  };
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon color="primary"/>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate autoComplete="off">
+            
+            <TextField
+              value={username}
+              onChange={handleUsername}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              value={password}
+              onChange={handlePassword}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleSignInButton}
+            >
+              Sign in
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign up."}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,197 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        CellarClub.com
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    },
+    image: {
+    backgroundImage: "url(/static/images/toast1.jpg)",
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    },
+    
+  paper: {
+    height: '100vh',
+    //margin: theme.spacing(4,4),
+    padding: theme.spacing(4,4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp(props) {
+  const classes = useStyles();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmation, setConfirmation] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmation = (e) => {
+    setConfirmation(e.target.value);
+  };
+
+  const handleSignUpButton = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      })
+    }
+    fetch('/api/signin', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      if (data.success) {
+        props.parentCallback(true)
+        props.history.push('/') 
+      } else {
+        setUsername('');
+        setPassword('');
+        alert(data.message)
+      }
+      
+    });
+  };
+
+    return (
+        <Grid container component="main" classname={classes.root}>
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>   
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon color="primary"/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <form className={classes.form} noValidate autoComplete="off">  
+                        <TextField
+                        value={username}
+                        onChange={handleUsername}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        type="text"
+                        name="username"
+                        autoComplete="username"
+                        autoFocus
+                        />
+                        <TextField
+                        value={email}
+                        onChange={handleEmail}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        type="Email"
+                        name="email"
+                        autoComplete="email"
+                        />
+                        <TextField
+                        value={password}
+                        onChange={handlePassword}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        />
+                        <TextField
+                        value={confirmation}
+                        onChange={handleConfirmation}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="confirmation"
+                        label="Confirmation"
+                        type="password"
+                        id="confirmation"
+                        autoComplete="current-confirmation"
+                        />
+                        <Button
+                        //type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={handleSignUpButton}
+                        >
+                        Sign up
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <Link href="/signin" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                    <Box mt={5}>
+                        <Copyright />
+                    </Box>
+                </div>
+            </Grid>
+        </Grid>
+    );
+}
