a7905927c751ad4fcdcc48e5250925f627559c51
duckbox72 <duckbox@hotmail.com>
2021-02-08 21:24:51 -0300

    landing updates

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,46 +3,65 @@ import Grid from '@material-ui/core/Grid';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Button, fade } from '@material-ui/core';
+import grey from '@material-ui/core/colors/grey'
 
-
+const test = grey[300];
 
 const useStyles = makeStyles((theme) => ({
-    root_container: {
+    root: {
         height: "100vh",
-        flex: 1,
+        flexGrow: 1,
         backgroundImage: "url(/static/images/barrels.jpg)",
         [theme.breakpoints.down("sm")]: {
             backgroundImage: "url(/static/images/toast.jpg)",
         },
         backgroundSize: 'cover',
         backgroundPosition: 'center',
+        textAlign: 'center',
+        
     },
     
     appbar: {
         backgroundColor: fade(theme.palette.secondary.dark, 0.85),
     },
-    
-    body_container: {
-        border: '2px solid red',
+    appbar_button: {
+        color: fade(theme.palette.common.white, 0.75),
     },
-    
-    button: {
-        color: fade(theme.palette.common.white , 0.75),
-    },
-    
-    img: {
+    appbar_img: {
         height: "60px",
         [theme.breakpoints.down("sm")]: {
             height: "50px",
         },
+    },
+    appbar_typography: {
+        flex: 1,
+    },
+    body_container: {
+    
+        //border: '2px solid red',
+        //height: '70vh'
+        
+    },
+    body_paper: {
+        backgroundColor: fade(theme.palette.common.white, 0.50),
+        padding: theme.spacing(2),
+        height: '50vh',
 
     },
-    
-    toolbar_typography: {
-        flex: 1,
+    body_typography: {
+        fontWeight: theme.typography.fontWeightMedium,
+        margin: theme.spacing(2),
+        color: theme.palette.grey[900],
+    },
+    body_button: {
+        borderRadius: 0,
+        fontWeight: theme.typography.fontWeightMedium,
+        padding: theme.spacing(2, 6),
+        boxShadow: 'none',
     },
 }));
 
@@ -51,38 +70,49 @@ export default function Landing(props) {
     const classes = useStyles();
 
     return (
-        <Grid container spacing={3} direction="column">
-            <Grid item xs={12}>
-                <AppBar position="static" className={classes.appbar}>
-                  <Toolbar  >
-                    <img src="/static/images/banner_no_bg.png" alt="logo" className={classes.img}></img>
-                    <Typography className={classes.toolbar_typography} />                 
-                    <Button variant="text" className={classes.button} href="/signin">
-                        Sign In
-                    </Button>
-                    <Button variant="text" className={classes.button} href="/signup">
-                        Sign Up
-                    </Button>
-                  </Toolbar>
-                </AppBar>
-            </Grid>    
-                
-            <Grid item container direction="column"
-                spacing={2} 
-                justify="center"
-                alignContent="center"
-                >
-                <Grid item className={classes.body_container}>
-                    <Button variant="contained" color="primary">
-                        Register
-                    </Button>
-                </Grid>
-                <Grid item >
-                    <Button variant="contained" color="primary">
-                        Register
-                    </Button>
-                </Grid>
+        <div className={classes.root}>
+            <Grid container spacing={8} direction="column">
+                <Grid item container xs={12}>
+                    <AppBar position="static" className={classes.appbar}>
+                    <Toolbar  >
+                        <img src="/static/images/banner_no_bg.png" alt="logo" className={classes.appbar_img}></img>
+                        <Typography className={classes.appbar_typography} />                 
+                        <Button variant="text" className={classes.appbar_button} href="/signin">
+                            Sign In
+                        </Button>
+                        <Button variant="text" className={classes.appbar_button} href="/signup">
+                            Sign Up
+                        </Button>
+                    </Toolbar>
+                    </AppBar>
+                </Grid>    
+                <div className={classes.body_container}>
+                    <Paper className={classes.body_paper}>      
+                        <Grid item container 
+                            spacing={2} 
+                            direction="column"
+                            justify="space-between"
+                            
+                            >
+                            <Grid item>
+                                <Typography variant="h3" className={classes.body_typography}>
+                                    COLLECTION MANAGER
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h5" className={classes.body_typography}>
+                                    
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="contained" color="primary" className={classes.body_button}>
+                                    Register
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </div>
             </Grid>
-        </Grid>
+        </div>
     );
 }
