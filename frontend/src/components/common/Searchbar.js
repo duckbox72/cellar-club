46bfa26c77e2025bc6ee0146b39a6bd5c4c095c5
duckbox72 <duckbox@hotmail.com>
2021-02-12 17:39:32 -0300

    Searchbar refactored from Class component to Functional component for consistency

--- frontend/src/components/common/Searchbar.js
+++ frontend/src/components/common/Searchbar.js
@@ -1,69 +1,41 @@
-import React, { Component } from "react";
+// THIS FILE WAS REFACTORED FROM A CLASS COMPOMNENT TO A FUNCTIONAL COMPONENT WITH HOOKS
+import React, { useState } from "react";
 import TextField from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid'
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 
-export default class Searchbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchChange: "",
-            searchTop10: []
-
-        }
-        this.handleSearchChange = this.handleSearchChange.bind(this);
-    }
-
-    handleSearchChange(e) {
-        this.setState({
-            searchChange: e.target.value,
-        });
-
+export default function Searchbar() {
+    
+    const [searchbarChange, setSearchbarChange] = useState("")
+    const [searchResult, setSearchResult] = useState([])
+    
+    const handleSearchbarChange = e => {
+        setSearchbarChange(e.target.value)
         console.log(e.target.value)
 
         fetch("/api/search" + "?display_name=" + e.target.value)
         .then((response) => response.json())
         .then(data => {
-            console.log(data)
-            this.setState({
-                searchTop10: data,
-            });
+            setSearchResult(data);
+        });  
+    }
 
+    return (
+    <div>
+        <Autocomplete
+            id="searchbar"
+            freeSolo
+            options={searchResult.map((option) => option.display_name)}
+            renderInput={(params) => (
+            <TextField 
+            {...params}
+            onChange={handleSearchbarChange} 
+            label="wine search" 
+            margin="normal" 
             
-        });
-        
-    }
-
-    render() {
-        return (
-        <div>
-            <Grid container spacing={1}>
-                <Grid item xs={12} align="center">
-                    <TextField
-                        onChange={this.handleSearchChange}
-                        label="Wine Search"
-                        placeholder="Enter wine name"
-                        value={this.state.searchChange}
-                        variant="outlined"
-                    />
-                </Grid>       
-            </Grid>
-            <Autocomplete
-                id="free-solo-demo"
-                freeSolo
-                options={this.state.searchTop10.map((option) => option.display_name)}
-                renderInput={(params) => (
-                <TextField 
-                {...params}
-                onChange={this.handleSearchChange} 
-                label="wine search" 
-                margin="normal" 
-                
-                variant="outlined" />
-                )}
-            />
-        </div>
-        )
-    }
-}
+            variant="outlined" />
+            )}
+        />
+    </div>
+    )
+};
