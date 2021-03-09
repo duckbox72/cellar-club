b7a9bc2566e1d50d6e7bb737caf2968d3582436b
duckbox72 <duckbox@hotmail.com>
2021-03-09 10:29:38 -0300

    class Bottle updated, migrations performed

--- api/models.py
+++ api/models.py
@@ -8,65 +8,6 @@ class User(AbstractUser):
     pass
 
 
-class Location(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cellar = models.CharField(max_length=64)
-    bin = models.CharField(max_length=64)
-    
-
-    def serializer(self):
-        return {
-            "user_id": self.user_id,
-            "cellar": self.cellarname,
-            "bin": self.bin,
-        }
-
-
-class BottleSize(models.Model):
-    size = models.CharField(max_length=8)
-
-    def serializer(self):    
-        return {
-            "size": self.size,
-        }
-
-
-class Vintage(models.Model):
-    vintage = models.CharField(max_length=4) 
-
-    def serializer(self):
-        return {
-            "vintage": self.vintage
-        }
-
-
-class Bottle(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
-    location = models.ForeignKey("Location", on_delete=models.SET_NULL, null=True, blank=True)
-    lwin = models.ForeignKey("Lwin", on_delete=models.SET_NULL, null=True, blank=True)
-    vintage = models.CharField(max_length=4)
-    gws = models.CharField(max_length=6)
-    store = models.CharField(max_length=64)
-    cost = models.IntegerField(default=0)
-    note = models.CharField(max_length=128)
-    date_added = models.DateTimeField(default=timezone.now)
-    consumed = models.BooleanField(default=False)
-
-    def serializer(self):
-        return {
-            "user": self.user,
-            "cellar": self.cellar,
-            "bin": self.bin,
-            "lwin": self.lwin,
-            "vintage": self.vintage,
-            "gws": self.gws,
-            "store": self.store,
-            "cost": self.cost,
-            "note": self.note,
-            "date_added": self.date_added,
-        }
-
-
 class Lwin(models.Model):
     lwin = models.CharField(max_length=8, blank=False, null=False, unique=True)
     display_name = models.CharField(max_length=256)
@@ -113,5 +54,89 @@ class Lwin(models.Model):
             "designation": self.designation, 
             "classification": self.classification,
         }
+
+
+class Location(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cellar = models.CharField(max_length=64)
+    bin = models.CharField(max_length=64)
+    
+
+    def serializer(self):
+        return {
+            "user_id": self.user_id,
+            "cellar": self.cellarname,
+            "bin": self.bin,
+        }
+
+
+class BottleSize(models.Model):
+    size = models.CharField(max_length=8)
+
+    def serializer(self):    
+        return {
+            "size": self.size,
+        }
+
+
+class Vintage(models.Model):
+    vintage = models.CharField(max_length=4) 
+
+    def serializer(self):
+        return {
+            "vintage": self.vintage
+        }
+
+
+class Bottle(models.Model):
+    # retrieved data
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    cellar = models.CharField(max_length=64)
+    bin = models.CharField(max_length=64)
+    gws = models.CharField(max_length=6)
+    
+    lwin7 = models.CharField(max_length=7)
+    display_name = models.CharField(max_length=256)
+    producer_title = models.CharField(max_length=64)
+    producer_name = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    region = models.CharField(max_length=64)
+    colour = models.CharField(max_length=16)
+
+    # data entered by user
+    vintage = models.CharField(max_length=4)
+    size = size = models.CharField(max_length=8)
+    store = models.CharField(max_length=64)
+    cost = models.IntegerField(default=0)
+    note = models.CharField(max_length=128)
+    consumed = models.BooleanField(default=False)
+
+    # auto entered data
+    lwin11 = models.CharField(max_length=11)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def serializer(self):
+        return {
+            "user": self.user,
+            "cellar": self.cellar,
+            "bin": self.bin,
+            "gws": self.gws,
+            "lwin7": self.lwin7,
+            "display_name": self.display_name,
+            "producer_title": self.producer_title,
+            "producer_name": self.producer_name,
+            "country": self.country,
+            "region": self.region,
+            "vintage": self.vintage,
+            "size": self.vintage,
+            "store": self.store,
+            "cost": self.cost,
+            "note": self.note,
+            "lwin11": self.lwin11,
+            "date_added": self.date_added,
+        }
+
+
+
     
 
