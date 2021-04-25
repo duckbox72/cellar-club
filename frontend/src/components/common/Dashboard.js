6c9aa6669ce6e7463bd014a77c44cd5725138277
duckbox72 <duckbox@hotmail.com>
2021-04-25 17:42:02 -0300

    fix to Home style. filter added

--- frontend/src/components/common/Dashboard.js
+++ frontend/src/components/common/Dashboard.js
@@ -53,7 +53,7 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: mystyleprops => mystyleprops.backgroundColorSchemaA,
         elevation: 3,
         '&:hover': {
-            backgroundColor: mystyleprops => mystyleprops.backgroundColorSchemaAHover,
+            filter: 'opacity(85%)',
             elevation: 6,
         },
     },
@@ -86,7 +86,6 @@ export default function Dashboard(props) {
         colorSchemaA: darkMode ? theme.palette.primary.main : theme.palette.secondary.main,
         colorSchemaB: darkMode ? theme.palette.secondary.main : theme.palette.primary.main,
         backgroundColorSchemaA: darkMode ? brown[600] : theme.palette.common.white,
-        backgroundColorSchemaAHover: darkMode ? theme.palette.secondary.main : theme.palette.grey[100],
     }
     const classes = useStyles(mystyleprops);
 
