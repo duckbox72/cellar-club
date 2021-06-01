a808111160e4b687378480335f773f2a5c45f411
duckbox72 <duckbox@hotmail.com>
2021-06-01 18:05:20 -0300

    project name updated to cellarclub

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'burgundigger.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cellarclub.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
