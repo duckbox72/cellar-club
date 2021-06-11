550aee9b29369f7f322bf3fbb1ea7d6dcd2321e2
Luis Felipe Klaus <duckbox@hotmail.com>
2021-06-11 15:39:48 -0300

    final adjusts after submiting2

--- cellarclub/settings.py
+++ cellarclub/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.22', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
